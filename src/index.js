4d9b5bb6254ccc004a7efd48dff0e56519c3ebef
Flávio Augusto Teixeira <flavioteixeira1@gmail.com>
2025-11-18 18:55:09 -0300

    programa OK
    randomXp randomName

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
-let nome = "felipe";
-console.log(nome);
+let nomes = ["Gigante guerreiro de Aço", 
+    "Metamorfose Atômica", "Viscount Bullet", "Skyward Sage","Iron Aegis" ,
+    "Celestial Titan", "Quantum Falcon",
+    "Stellar Guardian"]
+
+    const randomHeroNumber = Math.floor(Math.random() * nomes.length);
+
+    let heroName = nomes[randomHeroNumber];
+
+    //console.log(heroName)
+
+    const randomXP = Math.floor(Math.random()* 13000)
+
+    //console.log(randomXP)
+    let nivel = "Ferro"
+
+    if(randomXP < 1000){
+        nivel = "Ferro"
+    } else if (randomXP > 1000 && randomXP <= 2000){nivel = "Bronze"}
+      else if (randomXP > 2000 && randomXP <= 5000){nivel = "Prata"}
+      else if (randomXP > 5000 && randomXP <= 7000){nivel = "Ouro"}
+      else if (randomXP > 7000 && randomXP <= 8000){nivel = "Platina Diamante"}
+      else if (randomXP > 8000 && randomXP <= 9000){nivel = "Ascendente"}
+      else if (randomXP > 9000 && randomXP <= 10000){nivel = "Imortal"}
+      else{nivel = "Radiante"}
+
+    console.log("O herói " + heroName + " está no nível de " + nivel )
